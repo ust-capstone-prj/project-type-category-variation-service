1a55f2936ccf2b8769648a1845973f82c29248a7
Abhinav Sudhakar <28253243+AbhinavSdr@users.noreply.github.com>
2025-01-22 23:08:09 +0530

    initial upload

---
+++ src/main/java/com/ust/capstone/project_type_category_variation_service/ProjectTypeCategoryVariationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ust.capstone.project_type_category_variation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTypeCategoryVariationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTypeCategoryVariationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ust/capstone/project_type_category_variation_service/ProjectTypeCategoryVariationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ust.capstone.project_type_category_variation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTypeCategoryVariationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
