e75377be4051636b5f795502173c9cec991a4e49
Abhinav Sudhakar <28253243+AbhinavSdr@users.noreply.github.com>
2025-01-23 22:18:51 +0530

    update annotations

--- src/main/java/com/ust/capstone/project_type_category_variation_service/service/ProjectTypeCategoryVariationService.java
+++ src/main/java/com/ust/capstone/project_type_category_variation_service/service/ProjectTypeCategoryVariationService.java
@@ -1,14 +1,14 @@
 package com.ust.capstone.project_type_category_variation_service.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ust.capstone.project_type_category_variation_service.dao.ProjectTypeCategoryVariationRepository;
 import com.ust.capstone.project_type_category_variation_service.dao.entity.ProjectTypeCategoryVariation;
 
-@RestController
-@RequestMapping("/api")
+@Service
 public class ProjectTypeCategoryVariationService {
 
     @Autowired
