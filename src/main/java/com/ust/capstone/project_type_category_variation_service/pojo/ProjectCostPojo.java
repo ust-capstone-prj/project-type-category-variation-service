5cb5514579d79e993e88d34c0ff5fcb96fc1a76f
Abinaya <spabinaya2003@gmail.com>
2025-01-28 15:58:20 +0530

    added controller

--- src/main/java/com/ust/capstone/project_type_category_variation_service/pojo/ProjectCostPojo.java
+++ src/main/java/com/ust/capstone/project_type_category_variation_service/pojo/ProjectCostPojo.java
@@ -1,11 +1,11 @@
 package com.ust.capstone.project_type_category_variation_service.pojo;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+// import jakarta.persistence.Column;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,7 +13,6 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
 public class ProjectCostPojo {
 
     // private Long ProjTypCatCostBreakdown_id;
